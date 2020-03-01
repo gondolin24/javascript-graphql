3755ef35c7677e2c68758e9609a2056dd3dd3dac
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-02-29 21:46:23 -0500

    rancom router

--- express-ql-app/routes/index.js
+++ express-ql-app/routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({name: 'Eduardo'})
 });
 
-router.get('')
 
 module.exports = router;
